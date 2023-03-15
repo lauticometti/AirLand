e5e0f458ebea3482e9fe1a1e2bb3598b3782dbbf
Nicolas <nicoastigarraga@gmail.com>
2023-03-15 14:24:24 -0300

    helpers and middlewares added

--- api/src/index.js
+++ api/src/index.js
@@ -2,7 +2,5 @@ const app = require('./app')
 require('dotenv').config()
 
 app.listen(process.env.PORT, (req, res) => {
-    console.log(`Server on PORT: ${process.env.PORT}`);;
+	console.log(`Server on PORT: ${process.env.PORT}`)
 })
-
-
