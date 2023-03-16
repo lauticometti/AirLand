696bff9d2d02e63df33313e850a9732fa57a897c
Joan Ignacio Gonzalez <trollocat@gmail.com>
2023-03-16 01:55:25 -0300

    feat(client/components): added Cards and Card (dumb) that actually use backend info

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -1,29 +1,34 @@
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { fetchUsers } from "../redux/slices/usersSlice";
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchShoes } from '../../redux/slices/shoesSlice'
+import { Card } from '../../components'
+import './Cards.css'
 
-function UserList() {
-  const shoes = useSelector((state) => state.shoes.shoes);
-  const status = useSelector((state) => state.shoes.status);
-  const error = useSelector((state) => state.shoes.error);
-  const dispatch = useDispatch();
+export function Cards() {
+	const shoes = useSelector(state => state.shoes.shoes)
+	const status = useSelector(state => state.shoes.status)
+	const error = useSelector(state => state.shoes.error)
+	const dispatch = useDispatch()
 
-  useEffect(() => {
-    dispatch(fetchUsers());
-  }, [dispatch]);
+	useEffect(() => {
+		dispatch(fetchShoes())
+	}, [dispatch])
 
-  if (status === "loading") {
-    return <div>Loading...</div>;
-  }
+	if (status === 'loading') {
+		return <div>Loading...</div>
+	}
 
-  if (status === "failed") {
-    return <div>Error: {error}</div>;
-  }
+	if (status === 'failed') {
+		return <div>Error: {error}</div>
+	}
 
-  return <div>
-    <h1>Shoes from API</h1>
-    {shoes.map(shoe=><p key={shoe.id}>{shoe.id}</p>)}
-  </div>;
+	return (
+		<div className='cardsContainer'>
+			<div className='cardsGrid'>
+				{shoes.sneakers.map(shoe => (
+					<Card key={shoe.id} id={shoe.id} PRECIO={shoe.PRECIO} NOMBRE={shoe.NOMBRE} DETALLE={shoe.DETALLE} />
+				))}
+			</div>
+		</div>
+	)
 }
-
-export default UserList;
