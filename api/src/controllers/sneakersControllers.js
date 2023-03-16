247feb17bffaf36c0092a1be16f278867e68bfb0
bilvinas.ivan@gmail.com <bilvinas.ivan@gmail.com>
2023-03-16 17:40:39 -0300

    [fixed] getSneakers - [update] DB 'Zapatillas'

--- api/src/controllers/sneakersControllers.js
+++ api/src/controllers/sneakersControllers.js
@@ -2,10 +2,10 @@ const { db } = require('../firebase')
 
 const getSnickers = async (req, res) => {
 	try {
-		const querySnap = await db.collection('PRUEBAS').get()
+		const querySnap = await db.collection('ZAPATILLAS').get()
 		const sneakers = querySnap.docs.map(doc => ({
 			id: doc.id,
-			...doc.data().toLowerCase()
+			...doc.data()
 		}))
 		res.status(200).json({
 			ok: true,
@@ -66,7 +66,7 @@ const getSnickersById = async (req, res) => {
 	// para mostrar una zapatilla por id
 	try {
 		const sneaker = await (
-			await db.collection('PRUEBAS').doc(req.params.id).get()
+			await db.collection('ZAPATILLAS').doc(req.params.id).get()
 		).data()
 		res.status(200).json({
 			ok: true,
@@ -80,7 +80,7 @@ const deleteSnickers = async (req, res) => {
 	// para mostrar una zapatilla por id
 	const { STATUS } = req.body
 	try {
-		await db.collection('PRUEBAS').doc(req.params.id).update({ STATUS })
+		await db.collection('ZAPATILLAS').doc(req.params.id).update({ STATUS })
 		res.status(200).json({
 			ok: true,
 			message: 'Succesfuly deleted!'
@@ -94,7 +94,7 @@ const deleteSnickers = async (req, res) => {
 }
 const patchSnickers = async (req, res) => {
 	try {
-		await db.collection('PRUEBAS').doc(req.params.id).update(req.body)
+		await db.collection('ZAPATILLAS').doc(req.params.id).update(req.body)
 		res.status(200).json({
 			ok: true,
 			message: 'Succesfuly updated!'
