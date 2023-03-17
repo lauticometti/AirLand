40e33cf395ba5aec73fba6045d7fb8fe30982d31
LisanRomero <lisanromero18@gmail.com>
2023-03-16 21:53:36 -0300

    (api/src/controllers/sortsControllers) ordenamientos completados

--- api/src/controllers/sortsControllers.js
+++ api/src/controllers/sortsControllers.js
@@ -1,14 +1,14 @@
 const sortByName = (arrSneakers, order) => {
   let orderedSneakers
   switch (order) {
-    case 'asc':
+    case 'A-Z':
       orderedSneakers = arrSneakers.sort((a, b) => {
         if (a.NAME.toLowerCase() > b.NAME.toLowerCase()) return 1
         if (a.NAME.toLowerCase() < b.NAME.toLowerCase()) return -1
         return 0
       })
       return orderedSneakers
-    case 'desc':
+    case 'Z-A':
       orderedSneakers = arrSneakers.sort((a, b) => {
         if (a.NAME.toLowerCase() < b.NAME.toLowerCase()) return 1
         if (a.NAME.toLowerCase() > b.NAME.toLowerCase()) return -1
