1f3bac9fa17692728b76adc0c3fbac06b4e6ff97
Lautaro Cometti <lautic003@gmail.com>
2023-03-17 09:27:42 -0300

    (api/src/controllers): some filterfunction didnt return any value, this was fixed.

--- api/src/controllers/filtersControllers.js
+++ api/src/controllers/filtersControllers.js
@@ -1,64 +1,62 @@
 const { db } = require('../firebase')
 
-const filterByName = async (name) => {
-  try {
-    const sneakers = await db.collection('ZAPATILLAS').get()
-    const sneakersArr = sneakers.docs.map(sneaker => ({
-      ...sneaker.data()
-    }))
-    if (!name) return sneakersArr
-    const filteredSneakers = sneakersArr.filter(sneaker => {
-      if (sneaker.NAME.toLowerCase().includes(name.toLowerCase())) return sneaker
-    })
-    return filteredSneakers
-  } catch (error) {
-    throw new Error('Error en la DB')
-  }
+const filterByName = async name => {
+	try {
+		const sneakers = await db.collection('ZAPATILLAS').get()
+		const sneakersArr = sneakers.docs.map(sneaker => ({
+			...sneaker.data()
+		}))
+		if (!name) return sneakersArr
+		const filteredSneakers = sneakersArr.filter(sneaker =>
+			sneaker.NAME.toLowerCase().includes(name.toLowerCase())
+		)
+		return filteredSneakers
+	} catch (error) {
+		throw new Error('Error en la DB')
+	}
 }
 
-const filterBySize = async (size) => {
-  try {
-    const sneakers = await db.collection('ZAPATILLAS').get()
-    const sneakersArr = sneakers.docs.map(sneaker => ({
-      ...sneaker.data()
-    }))
-    const filteredSneakers = sneakersArr.filter(sneaker => {
-      if (sneaker.SIZE[size] > 0) return sneaker;
-    })
-    return filteredSneakers
-  } catch (error) {
-    throw new Error('Error en la DB');
-  }
+const filterBySize = async size => {
+	try {
+		const sneakers = await db.collection('ZAPATILLAS').get()
+		const sneakersArr = sneakers.docs.map(sneaker => ({
+			...sneaker.data()
+		}))
+		const filteredSneakers = sneakersArr.filter(
+			sneaker => sneaker.SIZE[size] > 0
+		)
+		return filteredSneakers
+	} catch (error) {
+		throw new Error('Error en la DB')
+	}
 }
 
-const filterByType = async (type) => {
-  try {
-    const sneakers = await db.collection('ZAPATILLAS').get()
-    const sneakersArr = sneakers.docs.map(sneaker => ({
-      ...sneaker.data()
-    }))
-    if (!type) return sneakersArr
-    const filteredSneakers = sneakersArr.filter(sneaker => {
-      if (sneaker.TYPE.toLowerCase() === type) return sneaker
-    })
-    return filteredSneakers
-  } catch (error) {
-    throw new Error('Error en la DB')
-  }
+const filterByType = async type => {
+	try {
+		const sneakers = await db.collection('ZAPATILLAS').get()
+		const sneakersArr = sneakers.docs.map(sneaker => ({
+			...sneaker.data()
+		}))
+		if (!type) return sneakersArr
+		const filteredSneakers = sneakersArr.filter(
+			sneaker => sneaker.TYPE.toLowerCase() === type
+		)
+		return filteredSneakers
+	} catch (error) {
+		throw new Error('Error en la DB')
+	}
 }
 
 const noFilters = async () => {
-  try {
-    const sneakers = await db.collection('ZAPATILLAS').get()
-    const sneakersArr = sneakers.docs.map(sneaker => ({
-      ...sneaker.data()
-    }))
-    return sneakersArr
-  } catch (error) {
-    throw new Error('Error en la DB')
-  }
+	try {
+		const sneakers = await db.collection('ZAPATILLAS').get()
+		const sneakersArr = sneakers.docs.map(sneaker => ({
+			...sneaker.data()
+		}))
+		return sneakersArr
+	} catch (error) {
+		throw new Error('Error en la DB')
+	}
 }
-
-
 
 module.exports = { filterByName, filterBySize, filterByType, noFilters }
--- api/src/controllers/sortsControllers.js
+++ api/src/controllers/sortsControllers.js
@@ -1,40 +1,40 @@
 const sortByName = (arrSneakers, order) => {
-  let orderedSneakers
-  switch (order) {
-    case 'asc':
-      orderedSneakers = arrSneakers.sort((a, b) => {
-        if (a.NAME.toLowerCase() > b.NAME.toLowerCase()) return 1
-        if (a.NAME.toLowerCase() < b.NAME.toLowerCase()) return -1
-        return 0
-      })
-      return orderedSneakers
-    case 'desc':
-      orderedSneakers = arrSneakers.sort((a, b) => {
-        if (a.NAME.toLowerCase() < b.NAME.toLowerCase()) return 1
-        if (a.NAME.toLowerCase() > b.NAME.toLowerCase()) return -1
-        return 0
-      })
-      return orderedSneakers
-    default:
-      arrSneakers
-  }
+	let orderedSneakers
+	switch (order) {
+		case 'asc':
+			orderedSneakers = arrSneakers.sort((a, b) => {
+				if (a.NAME.toLowerCase() > b.NAME.toLowerCase()) return 1
+				if (a.NAME.toLowerCase() < b.NAME.toLowerCase()) return -1
+				return 0
+			})
+			return orderedSneakers
+		case 'desc':
+			orderedSneakers = arrSneakers.sort((a, b) => {
+				if (a.NAME.toLowerCase() < b.NAME.toLowerCase()) return 1
+				if (a.NAME.toLowerCase() > b.NAME.toLowerCase()) return -1
+				return 0
+			})
+			return orderedSneakers
+		default:
+			return arrSneakers
+	}
 }
 
 const sortByPrice = (arrSneakers, order) => {
-  let orderedSneakers
-  switch (order) {
-    case 'asc':
-      orderedSneakers = arrSneakers.sort((a, b) => a.PRICE - b.PRICE)
-      return orderedSneakers
-    case 'desc':
-      orderedSneakers = arrSneakers.sort((a, b) => b.PRICE - a.PRICE)
-      return orderedSneakers
-    default:
-      arrSneakers
-  }
+	let orderedSneakers
+	switch (order) {
+		case 'asc':
+			orderedSneakers = arrSneakers.sort((a, b) => a.PRICE - b.PRICE)
+			return orderedSneakers
+		case 'desc':
+			orderedSneakers = arrSneakers.sort((a, b) => b.PRICE - a.PRICE)
+			return orderedSneakers
+		default:
+			return arrSneakers
+	}
 }
 
 module.exports = {
-  sortByName,
-  sortByPrice
+	sortByName,
+	sortByPrice
 }
