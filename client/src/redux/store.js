cdf3fdd40350267d671bca5272b04f8605283ce1
bilvinas.ivan@gmail.com <bilvinas.ivan@gmail.com>
2023-03-17 23:56:43 -0300

    [install] Firebase [add] front firebase config [add] firebase providers [add] /login and /signup routes [add] login and signup pages [add] login and signup components [add] authReducer for redux at store [add] authSlice [add] authThunks [delete] authentication.jsx

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { authSlice } from './slices'
 import shoeReducer, { fetchShoes } from './slices/shoesSlice'
 
 const store = configureStore({
 	reducer: {
-		shoes: shoeReducer
+		shoes: shoeReducer,
+		auth: authSlice.reducer
 	}
 })
 
