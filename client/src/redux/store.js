c35b068b5abea1ff322f289163af8bdcc1a3bf91
Lautaro Cometti <lautic003@gmail.com>
2023-03-19 18:45:52 -0300

    (client/src/redux): create a slice for handle the filters

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { authSlice } from './slices'
+import { authSlice, filterSlice } from './slices'
 import { shoesApi } from './services/services'
 import { setupListeners } from '@reduxjs/toolkit/dist/query'
 
 export const store = configureStore({
 	reducer: {
 		[shoesApi.reducerPath]: shoesApi.reducer,
+		filter: filterSlice.reducer,
 		auth: authSlice.reducer
 	},
 	middleware: getDefaultMiddleware =>
