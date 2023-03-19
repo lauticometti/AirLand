8aedb67927755e34df52a6e764ad933d1d9c0d83
Lautaro Cometti <lautic003@gmail.com>
2023-03-19 12:25:08 -0300

    (client/src/pages): now Detail and Home pages show the footer.

--- client/src/pages/Detail/Detail.jsx
+++ client/src/pages/Detail/Detail.jsx
@@ -1,5 +1,5 @@
 import { useParams } from 'react-router-dom'
-import { Loader, Navbar, NotFound } from '../../components'
+import { Footer, Loader, Navbar, NotFound } from '../../components'
 import { useGetShoesByIdQuery } from '../../redux/services/services'
 import styles from './Detail.module.css'
 
@@ -7,49 +7,62 @@ export function Detail() {
 	const { shoeId } = useParams()
 	const { data: shoe, isLoading, error } = useGetShoesByIdQuery(shoeId)
 
-	if (isLoading) return <Loader />
-	if (error) return <NotFound />
-
 	return (
 		<>
 			<Navbar />
 
-			<div className={styles.shoeContainer}>
-				<img
-					src={shoe.sneaker.IMAGE[0]}
-					alt={shoe.sneaker.NAME}
-					className={styles.image}
-				/>
-				<div className={styles.descriptionContainer}>
-					<h3 className={styles.title}>{shoe.sneaker.NAME}</h3>
-					<span className={styles.code}>Item nr: {shoe.sneaker.CODE}</span>
-					<h2 className={styles.shoePrice}>${shoe.sneaker.PRICE}</h2>
-					<div className={styles.sizesContainer}>
-						<p className={styles.sizesTitle}>Sizes</p>
-						<ul className={styles.sizesChecks}>
-							{Object.keys(shoe.sneaker.SIZE).map(size => {
-								return (
-									<li
-										key={size}
-										className={
-											shoe.sneaker.SIZE[size]
-												? styles.sizeCheck
-												: styles.sizeDisabled
-										}
-									>
-										<span className={styles.sizeSpan}>{size}</span>
-									</li>
-								)
-							})}
-						</ul>
+			<div className={styles.mainContainer}>
+				{isLoading ? (
+					<div className={styles.loader}>
+						<Loader />
 					</div>
-					<button className={styles.addToCartButton}>Add to cart</button>
-					<div className={styles.description}>
-						<h4 className={styles.descriptionh4}>Description</h4>
-						<p className={styles.descriptionText}>{shoe.sneaker.DESCRIPTION}</p>
+				) : error ? (
+					<div className={styles.error}>
+						<NotFound />
 					</div>
-				</div>
+				) : (
+					<div className={styles.shoeContainer}>
+						<img
+							src={shoe.sneaker.IMAGE[0]}
+							alt={shoe.sneaker.NAME}
+							className={styles.image}
+						/>
+						<div className={styles.descriptionContainer}>
+							<h3 className={styles.title}>{shoe.sneaker.NAME}</h3>
+							<span className={styles.code}>Item nr: {shoe.sneaker.CODE}</span>
+							<h2 className={styles.shoePrice}>${shoe.sneaker.PRICE}</h2>
+							<div className={styles.sizesContainer}>
+								<p className={styles.sizesTitle}>Sizes</p>
+								<ul className={styles.sizesChecks}>
+									{Object.keys(shoe.sneaker.SIZE).map(size => {
+										return (
+											<li
+												key={size}
+												className={
+													shoe.sneaker.SIZE[size]
+														? styles.sizeCheck
+														: styles.sizeDisabled
+												}
+											>
+												<span className={styles.sizeSpan}>{size}</span>
+											</li>
+										)
+									})}
+								</ul>
+							</div>
+							<button className={styles.addToCartButton}>Add to cart</button>
+							<div className={styles.description}>
+								<h4 className={styles.descriptionh4}>Description</h4>
+								<p className={styles.descriptionText}>
+									{shoe.sneaker.DESCRIPTION}
+								</p>
+							</div>
+						</div>
+					</div>
+				)}
 			</div>
+
+			<Footer />
 		</>
 	)
 }
