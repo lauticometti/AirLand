c61f57bcfb7b90605c05185526d129b31a82d48e
Joan Ignacio Gonzalez <trollocat@gmail.com>
2023-03-22 13:01:58 -0300

    fix(client/pages): form is now controlled, added error sweetalert

--- client/src/pages/Contact/Contact.jsx
+++ client/src/pages/Contact/Contact.jsx
@@ -6,31 +6,53 @@ import {
 	GoogleMap,
 	WhatsAppButton
 } from '../../components'
-import React from 'react'
 import emailjs from '@emailjs/browser'
 import swal from 'sweetalert'
+import { useState } from 'react'
+
+const formData = {
+	name: '',
+	email: '',
+	message: ''
+}
 
 export function Contact() {
+	const [form, setForm] = useState(formData)
+	const handleChange = event => {
+		setForm({
+			...form,
+			[event.target.name]: event.target.value
+		})
+	}
 	const sendEmail = event => {
 		event.preventDefault()
-
-		emailjs
-			.sendForm(
-				'service_4pkdd5m',
-				'template_0ui2buc',
-				event.target,
-				'R7XgKtrSj-Vs5_RTL'
-			)
-			.then(response => console.log(response))
-			.catch(error => console.log(error))
+		if (form.name && form.email && form.message) {
+			mostrarAlerta(true)
+			emailjs
+				.sendForm(
+					'service_4pkdd5m',
+					'template_0ui2buc',
+					event.target,
+					'R7XgKtrSj-Vs5_RTL'
+				)
+				.then(response => console.log(response))
+				.catch(error => console.log(error))
+		} else mostrarAlerta(false)
 	}
-	const mostrarAlerta = () => {
-		swal({
-			title: 'Message sent',
-			text: 'We will contact you soon',
-			icon: 'success',
-			timer: '2000'
-		})
+	const mostrarAlerta = isSent => {
+		isSent
+			? swal({
+					title: 'Message sent',
+					text: 'We will contact you soon',
+					icon: 'success',
+					timer: '2000'
+			  })
+			: swal({
+					title: 'Error',
+					text: 'Please complete the fields before sending',
+					icon: 'error',
+					timer: '2500'
+			  })
 	}
 	return (
 		<div>
@@ -42,14 +64,27 @@ export function Contact() {
 				<h1>leave us a message</h1>
 				<form onSubmit={sendEmail}>
 					<label>Name</label>
-					<input type='text' name='name' />
+					<input
+						type='text'
+						name='name'
+						value={form.name}
+						onChange={handleChange}
+					/>
 					<label>Email</label>
-					<input type='email' name='email' />
+					<input
+						type='email'
+						name='email'
+						value={form.email}
+						onChange={handleChange}
+					/>
 					<label>Message</label>
-					<textarea name='message' rows='5' />
-					<button type='submit' onClick={() => mostrarAlerta()}>
-						Send
-					</button>
+					<textarea
+						name='message'
+						rows='5'
+						value={form.message}
+						onChange={handleChange}
+					/>
+					<button type='submit'>Send</button>
 				</form>
 			</div>
 			<WhatsAppButton />
