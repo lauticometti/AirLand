1ffe0df86dd9387cfd4990c0f2d326bea9998607
Joan Ignacio Gonzalez <trollocat@gmail.com>
2023-03-22 20:56:56 -0300

    fixed whatsapp numbers and text

--- client/src/components/WhatsAppButton/WhatsAppButton.jsx
+++ client/src/components/WhatsAppButton/WhatsAppButton.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import { FaWhatsapp } from 'react-icons/fa';
 import styles from './WhatsAppButton.module.css'
 
 export function WhatsAppButton () {
   return (
-    <a href="https://api.whatsapp.com/send?phone=1164602560&text=Hi!+I+need+more+information+about..." className={styles.whatsapp_float} target="_blank" rel="noopener noreferrer">
+    <a href="https://api.whatsapp.com/send?phone=5491164602560&text=Hi!+I+need+more+information+about..." className={styles.whatsapp_float} target="_blank" rel="noopener noreferrer">
       <FaWhatsapp size={28} color="#fff" />
     </a>
   );
