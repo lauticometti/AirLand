97026b00db64de64834796d3f0134cd0f510213e
Lautaro Cometti <lautic003@gmail.com>
2023-03-22 22:48:52 -0300

    (api): fix types filterController

--- api/helpers/filterMaster.js
+++ api/helpers/filterMaster.js
@@ -4,13 +4,14 @@ const filterMaster = (shoes, { sizes, types, minPrice, name }) => {
 		shoes = shoes.filter(el => el.SIZE[sizes] && el.SIZE[sizes] > 0)
 	}
 
-	// if (types) {
-	// 	types.forEach(type => type.toLowerCase())
-	// 	shoes = shoes.filter(el => {
-	// 		if (!el.TYPE) return true
-	// 		return types.includes(el.TYPE.toLowerCase())
-	// 	})
-	// }
+	if (types) {
+		types = types.split('_')
+		types = types.map(type => type.toLowerCase())
+		shoes = shoes.filter(el => {
+			if (!el.TYPE) return true
+			return types.includes(el.TYPE.toLowerCase())
+		})
+	}
 
 	if (minPrice) {
 		shoes = shoes.filter(el => Number(el.PRICE) > Number(minPrice))
