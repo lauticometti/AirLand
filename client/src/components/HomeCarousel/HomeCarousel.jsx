38ddce65af508c6e43069e9fd246e9bfa43043ed
Lautaro Cometti <lautic003@gmail.com>
2023-03-24 19:13:35 -0300

    (client/src): modified styles of Snkrs carousel

--- client/src/components/HomeCarousel/HomeCarousel.jsx
+++ client/src/components/HomeCarousel/HomeCarousel.jsx
@@ -8,22 +8,22 @@ import { Link } from 'react-router-dom'
 export function HomeCarousel() {
 	return (
 		<Carousel>
-			<Carousel.Item interval={500}>
+			<Carousel.Item>
 				<Link to='/detail/MZulqmUuiI3bezocwqry'>
 					<img className='d-block w-100' src={Lunar} alt='ACRONYM' />
 				</Link>
 			</Carousel.Item>
-			<Carousel.Item interval={500}>
+			<Carousel.Item>
 				<Link to='/detail/GJlGj1WrEDmwJdagmWO4'>
 					<img className='d-block w-100' src={Roc} alt='ROC-A-FELLA' />
 				</Link>
 			</Carousel.Item>
-			<Carousel.Item interval={500}>
+			<Carousel.Item>
 				<Link to='/detail/Rpxal8wZRLwJAsbz3pSy'>
 					<img className='d-block w-100' src={Billie} alt=' Billie Eilish' />
 				</Link>
 			</Carousel.Item>
-			<Carousel.Item interval={500}>
+			<Carousel.Item>
 				<Link to='/detail/oMF3232s1vGXPR9wEkhC'>
 					<img className='d-block w-100' src={Travis} alt='TRAVIS SCOTT' />
 				</Link>
