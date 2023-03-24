68a007d801213c684c72ee687de52e7ab4ea469c
Lautaro Cometti <lautic003@gmail.com>
2023-03-24 19:12:44 -0300

    (client/src/components): modified navbar styles. On snkrs page, the Link to this page is replaced for the searchbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,28 +1,29 @@
-import { Link } from 'react-router-dom'
-import logo from '../../assets/air_land-black.svg'
+import { Link, useLocation } from 'react-router-dom'
+import logo from '../../assets/air_land-white.svg'
 import styles from './Navbar.module.css'
 import {
 	AiOutlineUser,
 	AiOutlineShoppingCart,
-	AiOutlinePhone,
 	AiOutlineLogout
 } from 'react-icons/ai'
+import { SearchBar } from '../'
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
 import { getCart, startLogout } from '../../redux'
 
 export function Navbar() {
-	const onLogout = () => {
-		dispatch(startLogout())
-	}
+	const dispatch = useDispatch()
 	const { cart } = useSelector(state => state.cart)
 	const { status, uid } = useSelector(state => state.auth)
-	const dispatch = useDispatch()
-
+	const { pathname } = useLocation()
 	useEffect(() => {
 		if (!uid) return
 		dispatch(getCart(uid))
 	}, [status])
+
+	const handleLogout = () => {
+		dispatch(startLogout())
+	}
 
 	return (
 		<nav className={styles.nav}>
@@ -34,32 +35,33 @@ export function Navbar() {
 				</li>
 
 				<li className={styles.navCenteredElement}>
-					<Link to='/snkrs' className={styles.navLink}>
-						SNKRS
-					</Link>
+					{pathname === '/snkrs' ? (
+						<SearchBar />
+					) : (
+						<Link to='/snkrs' className={styles.navLink}>
+							SNKRS
+						</Link>
+					)}
 				</li>
 
 				<li>
 					<div className={styles.rightestLiContainer}>
-						<Link to='/contact' className={styles.navLink}>
-							<AiOutlinePhone />
-						</Link>
-						{
-							uid
-								? <Link to='/profile' className={styles.navLink}>
-									<AiOutlineUser />
-								</Link>
-								: <Link to='/login' className={styles.navLink}>
-									<AiOutlineUser />
-								</Link>
-						}
+						{uid ? (
+							<Link to='/profile' className={styles.navLink}>
+								<AiOutlineUser />
+							</Link>
+						) : (
+							<Link to='/login' className={styles.navLink}>
+								<AiOutlineUser />
+							</Link>
+						)}
 						<div className={styles.cartContainer}>
 							<Link to='/store' className={styles.navLink}>
 								<AiOutlineShoppingCart />
 								{cart.length ? <span>{cart.length}</span> : ''}
 							</Link>
 						</div>
-						<span className={styles.navLink} onClick={onLogout}>
+						<span className={styles.navLink} onClick={handleLogout}>
 							<AiOutlineLogout />
 						</span>
 					</div>
