a899b8ad7f1664b6664127e76024f49342376708
Lautaro Cometti <lautic003@gmail.com>
2023-03-24 12:51:41 -0300

    (client/src/components): edited some styles

--- client/src/components/WhatsAppButton/WhatsAppButton.jsx
+++ client/src/components/WhatsAppButton/WhatsAppButton.jsx
@@ -5,19 +5,11 @@ export function WhatsAppButton() {
 	return (
 		<a
 			href='https://api.whatsapp.com/send?phone=5491164602560&text=Hi!+I+need+more+information+about...'
-<<<<<<< HEAD
 			className={styles.whatsappFloat}
 			target='_blank'
 			rel='noopener noreferrer'
 		>
 			<FaWhatsapp size={36} color='#fff' />
-=======
-			className={styles.whatsapp_float}
-			target='_blank'
-			rel='noopener noreferrer'
-		>
-			<FaWhatsapp size={28} color='#fff' />
->>>>>>> develop
 		</a>
 	)
 }
