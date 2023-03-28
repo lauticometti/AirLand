98e78753d88fd1ebad49064d33573b9ca8d31c19
Lautaro Cometti <lautic003@gmail.com>
2023-03-28 17:48:41 -0300

    (client/src/pages/Profile): A user page that aims to be reactive to the client&#39;s navigation.  For this, it uses an unusual folder structure, where the main component (the page itself), houses in its folder each of the components that will be displayed on it. So far, the only layout ready is the one that shows the client&#39;s personal information, although it does not yet allow changes to be made to this data.

---
+++ client/src/pages/Profile/Account/PersonalData/PersonalData.jsx
@@ -0,0 +1,56 @@
+import { useSelector } from 'react-redux'
+import styles from './PersonalData.module.css'
+import rightArrowLarge from '../../../../assets/icons/right_arrow-large.svg'
+
+export default function PersonalData() {
+	const { displayName, email } = useSelector(state => state.auth)
+
+	const birthday = '2003/04/02'
+	const password = '20030402'
+	const gender = 'Male'
+
+	return (
+		<article className={styles.personalData}>
+			<h3 className={styles.myDataTitle}>My details</h3>
+			<p>Modify your personal details below to keep your account up to date.</p>
+			<div className={styles.myData}>
+				<h4 className={styles.myDataDetail}>Details</h4>
+				<p className={styles.myDataName}>{displayName}</p>
+				<p className={styles.myDataBirthday}>{birthday || null}</p>
+				<p className={styles.myDataGender}>{gender}</p>
+				<span>Edit</span>
+			</div>
+			<div className={styles.accessData}>
+				<h4 className={styles.accessDataTitle}>Login details</h4>
+				<h5 className={styles.accessDataMail}>Email</h5>
+				<p className={styles.email}>{email || null}</p>
+				<h5 className={styles.accessDataPassword}>Password</h5>
+				<p className={styles.email}>{password || null}</p>
+				<span>Edit</span>
+			</div>
+			<div className={styles.logout}>
+				<h5 className={styles.logoutTitle}>Log out from all web browsers</h5>
+				<p className={styles.logoutText}>
+					Choosing this option will log you out of all web browsers you have
+					used to access the Air Land website. To log in again, you will need to
+					enter your credentials.
+				</p>
+				<button className={styles.logoutButton}>
+					<span>Log Me Out</span>
+					<img src={rightArrowLarge} alt='-->' className={styles.logoutArrow} />
+				</button>
+			</div>
+			<div className={styles.delete}>
+				<h5 className={styles.deleteTitle}>Manage Account</h5>
+				<button className={styles.deleteButton}>
+					<span>Delete account</span>
+					<img src={rightArrowLarge} alt='-->' className={styles.deleteArrow} />
+				</button>
+				<p className={styles.deleteText}>
+					If you delete your account, you will no longer have access to the
+					information stored in your account
+				</p>
+			</div>
+		</article>
+	)
+}
---
+++ client/src/pages/Profile/Purchases/Purchases.jsx
@@ -0,0 +1,3 @@
+export default function Purchases() {
+	return <div>Purchases</div>
+}
