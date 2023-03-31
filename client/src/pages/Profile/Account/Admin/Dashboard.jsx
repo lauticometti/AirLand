809df67d2e37e5fec190e5a74b59773300642cea
LisanRomero <lisanromero18@gmail.com>
2023-03-31 15:32:33 -0300

    dashboard innit

---
+++ client/src/pages/Profile/Account/Admin/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            hola exe
+        </div>
+    );
+};
+
+export default Dashboard;
--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux'
 import { Footer, Navbar } from '../../components'
 import Account from './Account/Account'
 import Orders from './Orders/Orders'
+import Dashboard from './Account/Admin/Dashboard'
 import styles from './Profile.module.css'
 import logo from '../../assets/icons/air_land-black.svg'
 
@@ -14,7 +15,8 @@ export function Profile() {
 
 	const components = {
 		account: <Account />,
-		orders: <Orders />
+		orders: <Orders />,
+		dashboard: <Dashboard />
 	}
 
 	const handleTabList = event => {
