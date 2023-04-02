c6136163a1d1cbddd15d55008139e890c7564799
LisanRomero <lisanromero18@gmail.com>
2023-04-02 09:33:04 -0300

    try-catch added

--- api/src/controllers/emailControllers.js
+++ api/src/controllers/emailControllers.js
@@ -12,30 +12,38 @@ let transporter = nodemailer.createTransport({
 })
 // send mail with defined transport object
 const welcomeEmail = async () => {
-	let info = await transporter.sendMail({
-		from: `Welcome user ${process.env.MAIL}`, // sender address
-		to: user.email, // list of receivers
-		subject: 'Welcome to AirLand', // Subject line
-		// text: "Hello world?", // plain text body
-		html: `
-        <h2> Welcome to AirLand's family ${user.displayName} </h2>
-        <h3> We are very happy that you choose us for your future purshares </h3>
-        <h4> Go to our website by clicking here ${homeURL}$ </h4>
-        `
-	})
+	try {
+		let info = await transporter.sendMail({
+			from: `Welcome user ${process.env.MAIL}`, // sender address
+			to: user.email, // list of receivers
+			subject: 'Welcome to AirLand', // Subject line
+			// text: "Hello world?", // plain text body
+			html: `
+			<h2> Welcome to AirLand's family ${user.displayName} </h2>
+			<h3> We are very happy that you choose us for your future purshares </h3>
+			<h4> Go to our website by clicking here ${homeURL}$ </h4>
+			`
+		})
+	} catch (error) {
+		throw new Error(error.message)
+	}
 }
 const succesPurshare = async () => {
-	let info = await transporter.sendMail({
-		from: `Shopping succes ${process.env.MAIL}`, // sender address
-		to: user.email, // list of receivers
-		subject: 'Purchase successfully completed', // Subject line
-		// text: "Hello world?", // plain text body
-		html: `
-        <h2> Hello ${user.displayName}</h2>
-        <h3> You are receiving this email because your purchase at AirLand has been successfully completed.</h3>
-        <h5> Thank you for shopping with us</h5>
-        `
-	})
+	try {
+		let info = await transporter.sendMail({
+			from: `Shopping succes ${process.env.MAIL}`, // sender address
+			to: user.email, // list of receivers
+			subject: 'Purchase successfully completed', // Subject line
+			// text: "Hello world?", // plain text body
+			html: `
+			<h2> Hello ${user.displayName}</h2>
+			<h3> You are receiving this email because your purchase at AirLand has been successfully completed.</h3>
+			<h5> Thank you for shopping with us</h5>
+			`
+		})
+	} catch (error) {
+		throw new Error(error.message)
+	}
 }
 module.export = {
 	transporter,
