cbadd3aa2ff4819b1caecb981a57ab388852d325
Joan Ignacio Gonzalez <trollocat@gmail.com>
2023-04-03 19:57:04 -0300

    feat(client/pages/Profile/AdminDashboard): add Orders section

--- client/src/pages/Profile/AdminDashboard/Orders/Orders.jsx
+++ client/src/pages/Profile/AdminDashboard/Orders/Orders.jsx
@@ -1,3 +1,28 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getAllOrdersGlobal } from '../../../../redux'
+
 export function Orders() {
-	return <div>Orders</div>
+	const { allOrders } = useSelector(state => state.shopping)
+	const dispatch = useDispatch()
+	useEffect(() => {
+		dispatch(getAllOrdersGlobal())
+	}, [])
+	return (
+		<div>
+			{allOrders
+				? allOrders.map(purchase => (
+						<div key={purchase.id}>
+							<h2>Purchase N#{purchase.id}</h2>
+							{purchase.cart.map(product => (
+								<div key={product.id}>
+									<label>{product.name}</label>
+									<img src={product.image.FULL} height='128px' alt='' />
+								</div>
+							))}
+						</div>
+				  ))
+				: 'loading...'}
+		</div>
+	)
 }
