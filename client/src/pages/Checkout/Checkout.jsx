53d0ed6df91a1d8f7203b10b7290caedd4f746aa
ivanbvn <96452713+ivanbvn@users.noreply.github.com>
2023-04-05 09:45:01 -0300

    Revert "Feat/responsive home"

--- client/src/pages/Checkout/Checkout.jsx
+++ client/src/pages/Checkout/Checkout.jsx
@@ -15,7 +15,7 @@ export function Checkout() {
 		streetNumber: '',
 		cityName: '',
 		stateName: '',
-		countryName: 'Argentina'
+		countryName: ''
 	})
 
 	const handleCheckoutSubmit = event => {
@@ -36,101 +36,79 @@ export function Checkout() {
 			<div className={styles.formContainer}>
 				<h2 className={styles.title}>Checkout details</h2>
 				<form className={styles.form} onSubmit={handleCheckoutSubmit}>
-					<h3 className={styles.formTitle}>Shipping address</h3>
-					<section>
-						<label htmlFor='streetName' className={styles.cityLabel}>
-							Street Name
-						</label>
-						<input
-							type='text'
-							id='streetName'
-							className={styles.cityInput}
-							name='streetName'
-							value={form.streetName}
-							onChange={handleInputChange}
-						/>
-					</section>
-					<section>
-						<label htmlFor='streetNumber' className={styles.stateLabel}>
-							Street Number
-						</label>
-						<input
-							type='text'
-							id='streetNumber'
-							className={styles.stateInput}
-							name='streetNumber'
-							value={form.streetNumber}
-							onChange={handleInputChange}
-						/>
-					</section>
-
-					<section>
-						<label htmlFor='cityName' className={styles.phoneLabel}>
-							City
-						</label>
-						<input
-							type='text'
-							id='cityName'
-							className={styles.phoneInput}
-							name='cityName'
-							value={form.cityName}
-							onChange={handleInputChange}
-						/>
-					</section>
-					<section className={styles.selectContainer}>
-						<label htmlFor='addAddress_province'>
-							Province <span className={styles.asterisc}>*</span>
-						</label>
-						<select
-							name='stateName'
-							value={form.province}
-							onChange={handleInputChange}
-						>
-							<option value=''>Choose your province</option>
-							<option value='Buenos Aires'>Buenos Aires</option>
-							<option value='Catamarca'>Catamarca</option>
-							<option value='Chaco'>Chaco</option>
-							<option value='Chubut'>Chubut</option>
-							<option value='C.A.B.A.'>C.A.B.A.</option>
-							<option value='Cordoba'>Cordoba</option>
-							<option value='Corrientes'>Corrientes</option>
-							<option value='Entre Ríos'>Entre Ríos</option>
-							<option value='Formosa'>Formosa</option>
-							<option value='Jujuy'>Jujuy</option>
-							<option value='La Pampa'>La Pampa</option>
-							<option value='La Rioja'>La Rioja</option>
-							<option value='Mendoza'>Mendoza</option>
-							<option value='Misiones'>Misiones</option>
-							<option value='Neuquén'>Neuquén</option>
-							<option value='Río Negro'>Río Negro</option>
-							<option value='Salta'>Salta</option>
-							<option value='San Juan'>San Juan</option>
-							<option value='San Luis'>San Luis</option>
-							<option value='Santa Cruz'>Santa Cruz</option>
-							<option value='Santa Fe'>Santa Fe</option>
-							<option value='Santiago del Estero'>Santiago del Estero</option>
-							<option value='Tierra del Fuego'>Tierra del Fuego</option>
-							<option value='Tucumán'>Tucumán</option>
-						</select>
-					</section>
-					<section>
-						<label htmlFor='zipCode' className={styles.zipCodeLabel}>
-							Zip code
-						</label>
-						<input
-							type='text'
-							id='zipCode'
-							className={styles.zipCodeInput}
-							name='zipCode'
-							value={form.zipCode}
-							onChange={handleInputChange}
-						/>
-					</section>
-
-					<p htmlFor='countryName' className={styles.country}>
-						Country: <span>AR</span>
-					</p>
-
+					<h3 className={styles.formTitle}>Shipping adress</h3>
+					<label htmlFor='streetName' className={styles.cityLabel}>
+						Street Name
+					</label>
+					<input
+						type='text'
+						id='streetName'
+						placeholder='Street Name'
+						className={styles.cityInput}
+						name='streetName'
+						value={form.streetName}
+						onChange={handleInputChange}
+					/>
+					<label htmlFor='streetNumber' className={styles.stateLabel}>
+						Street Number
+					</label>
+					<input
+						type='text'
+						id='streetNumber'
+						placeholder='Street Number'
+						className={styles.stateInput}
+						name='streetNumber'
+						value={form.streetNumber}
+						onChange={handleInputChange}
+					/>
+					<label htmlFor='zipCode' className={styles.zipCodeLabel}>
+						Zip code
+					</label>
+					<input
+						type='text'
+						id='zipCode'
+						placeholder='Zip code'
+						className={styles.zipCodeInput}
+						name='zipCode'
+						value={form.zipCode}
+						onChange={handleInputChange}
+					/>
+					<label htmlFor='cityName' className={styles.phoneLabel}>
+						City
+					</label>
+					<input
+						type='text'
+						id='cityName'
+						placeholder='City'
+						className={styles.phoneInput}
+						name='cityName'
+						value={form.cityName}
+						onChange={handleInputChange}
+					/>
+					<label htmlFor='stateName' className={styles.nameLabel}>
+						State
+					</label>
+					<input
+						type='text'
+						id='stateName'
+						placeholder='State'
+						className={styles.nameInput}
+						name='stateName'
+						value={form.stateName}
+						onChange={handleInputChange}
+					/>
+					<label htmlFor='countryName' className={styles.addressLabel}>
+						Country
+					</label>
+					<input
+						type='text'
+						id='countryName'
+						placeholder='Country'
+						className={styles.addressInput}
+						name='countryName'
+						value={form.countryName}
+						onChange={handleInputChange}
+					/>
 					<button
 						className={styles.paymentButton}
 						disabled={Object.values(form).some(input => input === '')}
